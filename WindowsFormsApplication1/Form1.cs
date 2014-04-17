9597ae66ef1b7fc64c126c954b633559e87c0638
showmen15 <showmen15@poczta.onet.pl>
2014-04-17 22:20:04 +0200

    udp client

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,6 +18,16 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
             //fsdfds
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] odobrane;
+            UdpClient udp = new UdpClient(1234);
+            IPEndPoint end =  new IPEndPoint(IPAddress.Parse("127.0.0.1"),1234);
+
+            odobrane = udp.Receive(ref end);
 
         }
     }
